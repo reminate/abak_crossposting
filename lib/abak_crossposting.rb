5e00763cb0dc4c74e227d7840d0a020eeec1e14d
Наталья Ремизова <reminate@gmail.com>
2013-04-04 11:27:15 -0300

    Update abak_crossposting.rb

--- lib/abak_crossposting.rb
+++ lib/abak_crossposting.rb
@@ -3,6 +3,8 @@ require "abak_crossposting/vkontakte/publisher"
 require "abak_crossposting/facebook/publisher"
 require "abak_crossposting/vkontakte/statistics_collector"
 require "abak_crossposting/facebook/statistics_collector"
+require "abak_crossposting/facebook/oauth"
+require "abak_crossposting/vkontakte/oauth"
 
 module AbakCrossposting
   class APIError < StandardError; end
